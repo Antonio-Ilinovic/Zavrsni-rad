1eef0381c19fb57eef24b12d7383fd9ad4a862ec
Antonio <ai51494@fer.hr>
2021-11-09 23:50:20 +0100

    promjena implementacije networka
    uklonjen zadnji linearni sloj. Sada je zadnji konvolucijski sloj. Ovo bi trebalo omogućiti da se može trenirati sa oknom slike (image patch) veličine 9x9 piksela. Može se i evaluirati cijela slika. Tada je ulaz u network 3xHxW, a izlaz je HxWx64, što znači da za svaki piksel dobivamo 64 značajke.

--- network.py
+++ network.py
@@ -11,13 +11,10 @@ class Conv64Features(nn.Module):
         self.conv2 = nn.Conv2d(in_channels=64, out_channels=64, kernel_size=(3, 3), padding=1)
         self.conv3 = nn.Conv2d(in_channels=64, out_channels=64, kernel_size=(3, 3), padding=1)
         self.conv4 = nn.Conv2d(in_channels=64, out_channels=64, kernel_size=(3, 3), padding=1)
-        self.fc1 = nn.Linear(64 * 9 * 9, 64)
 
     def forward(self, x):
         x = torch.relu(self.conv1(x))
         x = torch.relu(self.conv2(x))
         x = torch.relu(self.conv3(x))
         x = torch.relu(self.conv4(x))
-        x = x.view(x.size(0), -1)
-        x = self.fc1(x)
         return x
