3d8b23a4045c9888f21ff37e586e1bca2d414c4b
Antonio <ai51494@fer.hr>
2021-11-24 22:19:46 +0100

    privremeni commit

--- Training.py
+++ Training.py
@@ -3,70 +3,74 @@ from torch import nn
 from torch.utils.data import DataLoader
 
 from PatchesDataset import PatchesDataset
-from network import Conv64Features
+from Network import Conv64Features
 
 
-hyperparameters = {
-    'max_disp': 192,
-    'batch_size': 128,
-    'lr': 0.01,
-    'features': 64,
-    'ksize': 3,
-    'padding': 0,
-    'stem_strides': 1,
-    'margin': 0.2,
-    'epochs': 14,
-}
+if __name__ == '__main__':
 
-device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
-print(device)
+    hyperparameters = {
+        'MAX_DISP': 192,
+        'BATCH_SIZE': 128,
+        'LR': 0.001,
+        'FEATURES': 64,
+        'KSIZE': 3,
+        'PADDING': 0,
+        'STEM_STRIDES': 1,
+        'MARGIN': 0.2,
+        'EPOCHS': 14,
+    }
 
-model = Conv64Features()
-model.to(device)
+    device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+    print(f"Training is done on: {device}")
 
-patches_dataset = PatchesDataset()
-patches_train_loader = DataLoader(dataset=patches_dataset, batch_size=hyperparameters.get('batch_size'), shuffle=True)
+    model = Conv64Features()
+    model.to(device)
 
+    patches_dataset = PatchesDataset()
+    patches_train_loader = DataLoader(dataset=patches_dataset, batch_size=hyperparameters.get('BATCH_SIZE'), shuffle=True)
 
-def reference_patch_close_to_positive_patch_more_than_m_from_negative(reference_output, positive_output, negative_output, m=0.5):
-    # parametri reference, positive i negative output su izlazi iz modela mreže.
-    # Oni su vektori dimenzija (batch_size x 64)
+    criterion = nn.TripletMarginLoss(margin=hyperparameters.get('margin'))
+    optimizer = torch.optim.Adam(params=model.parameters(), lr=hyperparameters.get('lr'))
 
-    # L(R, P, Q) = max(0, m + h(R | θ) · h(Q | θ) − h(R | θ) · h(P | θ))
-    # R - referentno okno (patch) sa lijeve slike
-    # P - "točno" okno na desnoj slici
-    # Q - "netočno" okno na desnoj slici
-    # h - izlaz modela uz parametre θ.
-    # Ovakva funkcija gubitka zahtjeva da je ugrađivanje referentnog okna
-    # bliže ugrađivanju pozitivnog primjera za najmanje m.
-    # Drugim riječima, funkcija gubitka implicira kako model uči samo za one primjere
-    # u kojem je sličnost R i P manja od sličnosti između R i Q za više od m.
-
-    # pošto nema dot-product za batch podataka, koristimo torch.bmm
-    # https://discuss.pytorch.org/t/dot-product-batch-wise/9746
-    batch_size = reference_output.shape[0]
-    # koristim view metodu kako bih pravilno posložio batcheve podataka i mogao napraviti dot product za batch podataka
-    dot_reference_positive = torch.sum(reference_output.view(batch_size, -1) * positive_output.view(batch_size, -1), axis=1)
-    dot_reference_negative = torch.sum(reference_output.view(batch_size, -1) * negative_output.view(batch_size, -1), axis=1)
-
-    similarity_tensor = m + dot_reference_negative - dot_reference_positive
-    # ova linija je ekvivalentna: max(0.0, vrijednost)
-    similarity_tensor[similarity_tensor < 0.0] = 0.0
-    # vraćam sumu sličnosti, jer loss treba biti skalar. Je li to dobro na ovaj način?
-    return similarity_tensor.sum()
+    loss_list = []
+    cost_list = []
+    BATCH_SIZE = hyperparameters.get('batch_size')
 
 
-#criterion = reference_patch_close_to_positive_patch_more_than_m_from_negative
-criterion = nn.TripletMarginLoss(margin=hyperparameters.get('margin'))
-optimizer = torch.optim.Adam(params=model.parameters(), lr=hyperparameters.get('lr'))
+def train(hyperparameters, model, optimizer, criterion, device, dataloader_train):
+    for epoch in range(hyperparameters.get('EPOCHS')):
+        try:
+            # epoch training
+            model.train()
 
-loss_list = []
-cost_list = []
-BATCH_SIZE = hyperparameters.get('batch_size')
+            for anchor_patch, positive_patch, negative_patch in dataloader_train:
+                # put patches data to device (GPU)
+                anchor_patch = anchor_patch.to(device)
+                positive_patch = positive_patch.to(device)
+                negative_patch = negative_patch.to(device)
+
+                optimizer.zero_grad()
+
+                # forward anchor, positive and negative patches through model
+                # and view it as a 2D vector of dimensions [BATCH_SIZE, NUMBER_OF_FEATURES]
+                model_forward = lambda patch : model(patch).view(patch.shape[0], -1)
+                anchor_model_output = model_forward(anchor_patch)
+                positive_model_output = model_forward(positive_patch)
+                negative_model_output = model_forward(negative_patch)
+
+                # calculate loss using loss function (torch.TripletMarginLoss)
+                loss = criterion(anchor_model_output, positive_model_output, negative_model_output)
+                # accumulate gradients
+                loss.backward()
+                # update optimizer parameters
+                optimizer.step()
+
+    torch.save(model, 'trained_model.pth')
+
 
 for epoch in range(hyperparameters.get('epochs')):
     if epoch == 10:
-        optimizer.param_groups[0]['lr'] = 0.001
+        optimizer.param_groups[0]['lr'] = 0.0001
 
     cost = 0.0
     print(f"epoch {epoch+1}/{hyperparameters.get('epochs')}")
@@ -75,9 +79,6 @@ for epoch in range(hyperparameters.get('epochs')):
 
     model.train()
     for reference_patch, positive_patch, negative_patch in patches_train_loader:
-        # ove linije su za printanje napretka epohe
-        if count_batches % 10 == 0:
-            print(BATCH_SIZE*count_batches/16000000)
         count_batches += 1
 
         reference_patch = reference_patch.to(device)
@@ -98,12 +99,13 @@ for epoch in range(hyperparameters.get('epochs')):
             negative_output.view(current_batch_size, -1))
         loss.backward()
         optimizer.step()
-        loss_list.append(loss.item())
+        #loss_list.append(loss.item())
         cost += loss.item()
+
+        if count_batches % 1000 == 0:
+            print(f"{BATCH_SIZE * count_batches / 16000000}  loss={loss.item()}")
 
     cost_list.append(cost)
     print(f" cost={cost}")
     torch.save(model, f"trained_model_{epoch}.pth")
 
-# spremam istrenirani model za kasniju upotrebu
-torch.save(model, 'trained_model.pth')
