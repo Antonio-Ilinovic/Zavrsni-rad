803b91ff479ae6842c6adc2dee20a3156dc2ddf0
Antonio Ilinović <antonio.ilinovic1@gmail.com>
2021-12-07 00:10:15 +0100

    dodane labele pri plotanju matplotlib-om

--- main_result_info.py
+++ main_result_info.py
@@ -19,24 +19,30 @@ def show_end_result_of_trained_model(model, num_image):
     real_disparity = Utils.get_disp_image(num_image)
     plt.subplot(2, 1, 1)
     plt.imshow(real_disparity)
+    plt.title(f'real disparity {num_image}')
     predicted_disparity = Evaluate.predict_disparity_map(num_image, model)
     plt.subplot(2, 1, 2)
     plt.imshow(predicted_disparity)
+    plt.title(f'predicted disparity {num_image}')
     plt.show()
 
     plot_accuracy_image_with_image_index(num_image, model)
+    plt.title(f"accuracy image {num_image}")
     plt.show()
 
     plt.subplot(2, 1, 1)
     plt.imshow(Utils.get_left_image(num_image))
+    plt.title(f"left image {num_image}")
     plt.subplot(2, 1, 2)
     plt.imshow(Utils.get_right_image(num_image))
+    plt.title(f"right image {num_image}")
     plt.show()
 
     train_loss = np.load("train_loss_per_epoch.npy")
     validation_loss = np.load("validation_loss_per_epoch.npy")
-    plt.plot(train_loss)
-    plt.plot(validation_loss)
+    plt.plot(train_loss, label='train loss')
+    plt.plot(validation_loss, label='validation loss')
+    plt.legend()
     plt.show()
 
 
